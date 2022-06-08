a52f477d3d36a82b12b75a9c2e8238662a4b8ed5
marcolangermg <marcolangermg@gmail.com>
2022-06-08 08:40:31 -0300

    pubsub endpoint optional

--- src/shared/pub-sub/domain/pub-sub-settings.ts
+++ src/shared/pub-sub/domain/pub-sub-settings.ts
@@ -2,9 +2,8 @@ import { OptionalEnv } from "@app/settings/environment-variables";
 import env from "env-var";
 
 export class PubSubSettings {
-  public readonly apiEndpoint = env
+  public readonly apiEndpoint? = env
     .get(OptionalEnv.PUB_SUB_API_ENDPOINT)
-    .default("http://pubSub:8085")
     .asUrlString();
 
   public readonly projectId = env
