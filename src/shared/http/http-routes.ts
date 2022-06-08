75a09cdc27fc446b4e3671f18f545edab3c8eaf9
marcolangermg <marcolangermg@gmail.com>
2022-06-08 08:54:42 -0300

    set api version to endpoints

--- src/shared/http/http-routes.ts
+++ src/shared/http/http-routes.ts
@@ -1,6 +1,6 @@
 export enum HttpRoutes {
   STATUS = "/status",
-  ACCOUNT_CREATE = "/account/create",
-  ACCOUNT_CREATE_ASYNC = "/account/create/async",
-  ACCOUNT_CREATE_PROCESS = "/account/create/process",
+  ACCOUNT_CREATE = "/api/v1/account/create",
+  ACCOUNT_CREATE_ASYNC = "/api/v1/account/create/async",
+  ACCOUNT_CREATE_PROCESS = "/api/v1/account/create/process",
 }
